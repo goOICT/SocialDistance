477a87c376f13f91a5cfb8958cf599c4c7320fa9
Aaron Cooley <acooley@kunaiconsulting.com>
2020-06-09 18:36:05 -0700

    Layout implemented.

---
+++ ios/Social Distance Alarm/Social Distance Alarm/Teams/TeamsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamsViewController.swift
+//  Social Distance Alarm
+//
+//  Created by Aaron Cooley on 6/9/20.
+//  Copyright © 2020 Kunai. All rights reserved.
+//
+
+import UIKit
+
+class TeamsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
