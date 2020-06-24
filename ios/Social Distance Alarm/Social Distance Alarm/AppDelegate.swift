5ac6e94252b5580e60ce1fb8cf72369531584013
Piotr Zmudzinski <ptr.zmudzinski@gmail.com>
2020-06-24 11:29:24 +0200

    assets update

--- ios/Social Distance Alarm/Social Distance Alarm/AppDelegate.swift
+++ ios/Social Distance Alarm/Social Distance Alarm/AppDelegate.swift
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
